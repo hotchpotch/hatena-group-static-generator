23184c9aa06b3232f1e5708407b708876d2e55a5
Yuichi Tateno <hotchpotch@gmail.com>
2020-03-31 00:12:17 +0900

    Split Entry, Add commentId

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default ({ author, text, date }) => {
+export default ({ author, text, date, commentId }) => {
     const authorLink = `https://www.hatena.ne.jp/${author}/`;
     const authorImage = `https://www.hatena.ne.jp/users/${author.slice(0, 2)}/${author}/profile_s.gif`;
     return (
@@ -9,8 +9,8 @@ export default ({ author, text, date }) => {
                 <a href={authorLink} className="hatena-id-icon">
                     <img src={authorImage} className="hatena-id-icon" alt={author} title={author} height="16" width="16" />{author}</a>
             </span>
-            <span className="timestamp">
-                <a href="#c">{date}</a>
+            <span className="timestamp" id={commentId}>
+                <a href={"#" + commentId}>{date}</a>
             </span>
             <span className="commentbody">{text}</span>
         </p>
--- src/templates/hatena-group-entry.js
+++ src/templates/hatena-group-entry.js
@@ -1,32 +1,21 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 
+import Entry from "../components/Entry"
 import Layout from "../components/Layout"
-import Comment from "../components/Comment"
 
 export default ({ data }) => {
   const { id, content, title, date, comments } = data.hatenaGroupContent;
   const headTitle = `${title} - ${data.site.siteMetadata.title}`
-  console.log(comments);
 
   return (
     <Layout>
       <Helmet>
         <title>{headTitle}</title>
       </Helmet>
-
-      <div className="day">
-        <h2><Link to={`/${id}`}><span className="date">{date}</span></Link></h2>
-        <div className="body" dangerouslySetInnerHTML={{ __html: content }} />
-
-        <div className="comment">
-          <div className="caption"><a href="#c">コメントは書けません</a></div>
-          <div class="commentshort">
-            {comments.map((comment, index) => <Comment key={index} {...comment} />)}
-          </div>
-        </div>
-      </div>
+      <Entry id={id} content={content} date={date} comments={comments} >
+      </Entry>
     </Layout>
   )
 }
