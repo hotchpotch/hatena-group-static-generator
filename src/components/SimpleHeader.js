5592f0f4baf792f2e6afeaa57a8eb12808c7080b
Yuichi Tateno <hotchpotch@gmail.com>
2020-03-31 15:08:38 +0900

    Change design at search result

--- src/components/SimpleHeader.js
+++ src/components/SimpleHeader.js
@@ -1,14 +1,12 @@
 import React from "react"
 import { Link } from "gatsby";
 import HeaderImages from "./HeaderImages";
+import SearchHeader from "./SearchHeader";
 
 export default () => (
     <div id="simple-header">
         <HeaderImages />
-        <form method="get" action="/search" className="search-form">
-            <input type="text" className="search-word" name="word" />
-            <input type="submit" name=".searchword" value="検索" className="search-button" />
-        </form>
+        <SearchHeader />
         <ul className="menu">
             <li><Link to="/">トップ</Link></li>
             <li><Link to="/">最新の日記</Link></li>
