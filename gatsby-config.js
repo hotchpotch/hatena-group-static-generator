71a67fcb2a20eb5a3e667652d40a58ea3e938cf6
Yuichi Tateno <hotchpotch@gmail.com>
2020-03-31 12:05:04 +0900

    Add gatsby-lugin-lunr && init settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,34 @@ module.exports = {
         name: `hatena-group`,
       },
     },
+    {
+      resolve: `gatsby-plugin-lunr`,
+      options: {
+        languages: [
+          {
+            name: 'ja',
+          },
+        ],
+        fields: [
+          { name: 'title', store: true, attributes: { boost: 20 } },
+          { name: 'content', store: true },
+          { id: 'id', store: true },
+        ],
+        resolvers: {
+          HatenaGroupContent: {
+            title: node => node.title,
+            content: node => node.text,
+            id: node => node.id,
+          },
+        },
+        //custom index file name, default is search_index.json
+        filename: 'search_index.json',
+        //custom options on fetch api call for search_ındex.json
+        fetchOptions: {
+          credentials: 'same-origin'
+        },
+      },
+    },
     'gatsby-plugin-eslint',
     `gatsby-plugin-react-helmet`,
   ]
