e0100cc579d0a0490cece6c71d162d6f418e6ce6
Yuichi Tateno <hotchpotch@gmail.com>
2020-03-31 09:47:43 +0900

    Move css files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
-import "./vendor/styles/base.css";
-import "./vendor/styles/headerstyle-color-de.css";
-import "./vendor/styles/hatena2.css";
+import "./vendor/hatena/styles/base.css";
+import "./vendor/hatena/styles/headerstyle-color-de.css";
+import "./vendor/hatena/styles/hatena2.css";
 
 import "./src/styles/global.css";
